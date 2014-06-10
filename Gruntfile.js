d8071d2d42b81fe60f3c85b581ad6b1f626f682b
stelioschar <stelioschar@gmail.com>
2014-06-10 23:44:06 +0300

    clean up gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,10 @@
 module.exports = function(grunt) {
 
   require('load-grunt-tasks')(grunt);
+
+  // measures the time each task takes
+  require('time-grunt')(grunt);
   
   require('load-grunt-config')(grunt);
-
-  grunt.registerTask('default', [
-    'watch',
-  ]);
 
 }
